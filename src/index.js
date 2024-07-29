36b5bce2dd1a29566b0572dc9181ea0b721990f1
Anton-Osip <osipchika@gmail.com>
2024-07-29 16:06:47 +0300

    feat: finished this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num;
+    if (n < 0) num = n * -1;
+    if (n >= 0) num = n;
+
+    let arr = [...(num + "")].reverse();
+
+    return Number(arr.join(""));
+};
